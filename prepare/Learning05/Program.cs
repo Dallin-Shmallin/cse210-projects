d24aa9463cfb01092b97135e0dd0e6fae7857db1
Dallin-Shmallin <94138441+Dallin-Shmallin@users.noreply.github.com>
2025-05-03 16:44:11 -0700

    learning 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -2,8 +2,37 @@ using System;
 
 class Program
 {
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name:");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number:");
+        int Number = Convert.ToInt32(Console.ReadLine());
+        return Number;
+    }
+    static int SquareNumber(int Number)
+    {
+        return Number * Number;
+    }
+    static void DisplayResult(string userName, int Number, int squaredNumber)
+    {
+        Console.WriteLine($"Well then {userName}, your favorite number is {Number}, and its square is unsurprisingly {squaredNumber}.");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int Number = PromptUserNumber();
+        int squaredNumber = SquareNumber(Number);
+        DisplayResult(userName, Number, squaredNumber);
     }
 }
