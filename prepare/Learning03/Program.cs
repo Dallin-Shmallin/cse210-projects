2fda7eaee5bec60ee4a564ed0aa35cb713186d24
Dallin-Shmallin <94138441+Dallin-Shmallin@users.noreply.github.com>
2025-05-03 09:10:55 -0700

    prep3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Console.WriteLine("What is the magic number?");
+        int magic_number = Convert.ToInt32(Console.ReadLine());
+
+        int guess;
+        do
+        {
+            Console.WriteLine("What is your guess?");
+            guess = Convert.ToInt32(Console.ReadLine());
+
+            if (guess > magic_number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess < magic_number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+        while (guess != magic_number);
     }
 }
