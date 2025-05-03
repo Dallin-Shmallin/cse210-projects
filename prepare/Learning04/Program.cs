2a8f4c75d34d2184d897269e4ec748342f0ee25f
Dallin-Shmallin <94138441+Dallin-Shmallin@users.noreply.github.com>
2025-05-03 09:28:25 -0700

    learning04 prep

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        int number;
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when you're finished:");
+        do{
+            Console.WriteLine("Enter a number:");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+        while (number != 0);
+        Console.WriteLine("The sum is: " + numbers.Sum());
+        Console.WriteLine("The average is: " + numbers.Average());
+        Console.WriteLine("The largest number is: " + numbers.Max());
     }
 }
