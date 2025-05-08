6b7b71adfe176e70a6b4934f3c6965dfe7373408
dallin_shmallin <dallin.mccaleb@gmail.com>
2025-05-08 14:21:38 -0600

    some changes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,20 @@
 using System;
+public class Job
+{
+    public string _company;
+    public int _startyear;
+    public int _endyear;
 
+    public string DisplayJobInformation(string company, int startyear, int endyear)
+    {
+        return string.Format("", company,startyear,endyear);
+    }
+}
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
     }
 }
