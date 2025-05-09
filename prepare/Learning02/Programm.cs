7b315ed711659a4e79fb4ac2400fd19c0d494a28
dallin_shmallin <dallin.mccaleb@gmail.com>
2025-05-08 18:58:52 -0600

    final prepare2

--- prepare/Learning02/Programm.cs
+++ prepare/Learning02/Programm.cs
@@ -4,11 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Job john = new Job();
-        john._company = "McDonalds";
-        john._startyear = 2025;
-        john._endyear = 2026;
-        john._jobTitle = "Fast Food Worker";
-        john.Display();
+        Job job1 = new Job();
+        job1._company = "McDonalds";
+        job1._startyear = 2025;
+        job1._endyear = 2026;
+        job1._jobTitle = "Fast Food Worker";
+
+                Job job2 = new Job();
+        job2._jobTitle = "epic supervisor";
+        job2._company = "Epic Games";
+        job2._startyear = 2023;
+        job2._endyear = 2026;
+
+        Resume employeeResume = new Resume();
+        employeeResume._name = "Bingo Epicus";
+
+        employeeResume._jobs.Add(job1);
+        employeeResume._jobs.Add(job2);
+
+        employeeResume.Display();
     }
 }
