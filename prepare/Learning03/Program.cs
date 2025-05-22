3fddfc3b4042d54583e8688789d016aaa79d4262
Dallin-Shmallin <94138441+Dallin-Shmallin@users.noreply.github.com>
2025-05-22 14:13:47 -0600

    idk

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,8 +1,14 @@
 using System;
 using Microsoft.VisualBasic;
+
 class Program
 {
-    Fraction fraction = new Fraction();
-    Fraction fraction2 = new Fraction(3);
-    Fraction fraction3 = new Fraction(3, 4);
+    static void Main()
+    {
+        Fraction fraction = new Fraction();
+        Fraction fraction2 = new Fraction(3);
+        Fraction fraction3 = new Fraction(3, 4); 
+    }
 }
+  
+    
