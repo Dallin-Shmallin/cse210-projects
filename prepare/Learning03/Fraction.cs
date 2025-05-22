7f283b2fe5646980c24161baee1a1531ff94ea5d
Dallin-Shmallin <94138441+Dallin-Shmallin@users.noreply.github.com>
2025-05-22 14:11:37 -0600

    idk

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,21 @@
+using System.Runtime.CompilerServices;
+class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+    public Fraction(int numerator)
+    {
+        _numerator = numerator;
+        _denominator = 1;
+    }
+    public Fraction(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,31 +1,8 @@
 using System;
-
+using Microsoft.VisualBasic;
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("What is the magic number?");
-        int magic_number = Convert.ToInt32(Console.ReadLine());
-
-        int guess;
-        do
-        {
-            Console.WriteLine("What is your guess?");
-            guess = Convert.ToInt32(Console.ReadLine());
-
-            if (guess > magic_number)
-            {
-                Console.WriteLine("Lower");
-            }
-            else if (guess < magic_number)
-            {
-                Console.WriteLine("Higher");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }
-        }
-        while (guess != magic_number);
-    }
+    Fraction fraction = new Fraction();
+    Fraction fraction2 = new Fraction(3);
+    Fraction fraction3 = new Fraction(3, 4);
 }
