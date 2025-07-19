e0af183a129e2bdd77fd65c4012af81fbae68cf9
Dallin-Shmallin <94138441+Dallin-Shmallin@users.noreply.github.com>
2025-07-19 14:21:37 -0600

    yay it works

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -8,19 +8,33 @@ class Program
         Console.WriteLine("Enter the filepath of the image to convert to ASCII art:");
         string imagePath = Console.ReadLine();
 
-        Console.WriteLine("Would you like this image to be in color or black and white? (c/b)");
+        Console.WriteLine("Do you want this image to be in color or black and white? (c/b)");
         string choice = Console.ReadLine();
 
         ImageManager imageManager = new ImageManager();
         Image image = imageManager.LoadImage(imagePath);
 
-        Pixel[,] pixels = imageManager.ParseToPixels(image);
-        PixelRow[] pixelRows = imageManager.CreatePixelRows(pixels);
-        PixelColumn[] pixelColumns = imageManager.CreatePixelColumns(pixels);
+        Pixel[] pixels = imageManager.ParseToPixels(image);
+        // make sure the image is 1080 by 1920
+        PixelRow[] pixelRows = imageManager.CreatePixelRows(pixels,1080, 1920);
+        PixelColumn[] pixelColumns = imageManager.CreatePixelColumns(pixels,1080,1920);
 
         PixelCluster[,] clusters = imageManager.CreatePixelClusters(pixelRows, pixelColumns);
 
-        Character[,] characters = imageManager.GenerateCharacters(clusters, choice == "c");
+        Character[,] characters;
+        if (choice.ToLower() == "b")
+        {
+            characters = imageManager.GenerateCharacters(clusters, false);
+        }
+        else if (choice.ToLower() == "c")
+        {
+            characters = imageManager.GenerateCharacters(clusters, true);
+        }
+        else
+        {
+            Console.WriteLine("uhh not sure what you entered so ima do black and white");
+            characters = imageManager.GenerateCharacters(clusters, false);
+        }
 
         string asciiArt = imageManager.ConvertCharactersToAscii(characters);
 
