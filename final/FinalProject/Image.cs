185f2f68cabbb238856313e90915dacb0cf09cf8
Dallin-Shmallin <94138441+Dallin-Shmallin@users.noreply.github.com>
2025-07-19 13:37:56 -0600

    idek

--- final/FinalProject/Image.cs
+++ final/FinalProject/Image.cs
@@ -1,11 +1,25 @@
+using System.Drawing;
+
 class Image
 {
     private string _filePath;
     public Image(string filePath) { _filePath = filePath; }
     public Pixel[,] GetPixels()
     {
-        // Load image and return 2D array of Pixels
-        // (Stub: implement with System.Drawing or ImageSharp)
-        return new Pixel[100, 100];
+        using (Bitmap bitmap = new Bitmap(_filePath))
+        {
+            int width = bitmap.Width;
+            int height = bitmap.Height;
+            Pixel[,] pixels = new Pixel[width, height];
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Color color = bitmap.GetPixel(x, y);
+                    pixels[x, y] = new Pixel(new Rgb(color.R, color.G, color.B));
+                }
+            }
+            return pixels;
+        }
     }
 }
--- final/FinalProject/PixelCluster.cs
+++ final/FinalProject/PixelCluster.cs
@@ -1,11 +1,48 @@
 class PixelCluster
 {
     private Pixel[] _pixels;
+    private int _clusterHeight = 10;
+    private int _clusterWidth = 10;
+
     public PixelCluster(PixelRow[] rows, PixelColumn[] columns, int rowIdx, int colIdx)
     {
-        // Gather pixels for this cluster from rows/columns
-        // (Stub: implement logic for cluster size and pixel selection)
-        _pixels = new Pixel[1]; // placeholder
+        int startRow = rowIdx * _clusterHeight;
+        int startCol = colIdx * _clusterWidth;
+
+        var pixelList = new System.Collections.Generic.List<Pixel>();
+        for (int r = startRow; r < startRow + _clusterHeight && r < rows.Length; r++)
+        {
+            Pixel[] rowPixels = rows[r].GetPixels();
+            for (int c = startCol; c < startCol + _clusterWidth && c < rowPixels.Length; c++)
+            {
+                pixelList.Add(rowPixels[c]);
+            }
+        }
+        _pixels = pixelList.ToArray();
     }
+
     public Pixel[] GetPixels() => _pixels;
+
+    public string ToAsciiChar()
+    {
+        if (_pixels.Length == 0) return " ";
+
+        // ASCII gradient from dark to light
+        string asciiChars = "@%#*+=-:. ";
+        double totalBrightness = 0;
+
+        foreach (var pixel in _pixels)
+        {
+            Rgb color = pixel.GetColor();
+            // weird formula to calculate brightness
+            double brightness = 0.299 * color.R + 0.587 * color.G + 0.114 * color.B;
+            totalBrightness += brightness;
+        }
+
+        double avgBrightness = totalBrightness / _pixels.Length;
+        int index = (int)(avgBrightness / 255.0 * (asciiChars.Length - 1));
+        index = asciiChars.Length - 1 - index;
+
+        return asciiChars[index].ToString();
+    }
 }
